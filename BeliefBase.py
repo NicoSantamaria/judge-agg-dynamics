aedfa2e1c03cc237693163ef5dde3de4710b450b
NicoSantamaria <nicoasantamaria@gmail.com>
2024-10-04 20:05:30 -0700

    generate models with constraints

--- BeliefBase.py
+++ BeliefBase.py
@@ -1,13 +1,19 @@
+from itertools import product
+
 type Sentence = list[chr]
-type Interpretations = tuple[int]
+type Interpretation = tuple[int]
 type Atoms = dict[chr, int]
 
 class BeliefBase:
     def __init__(self, propositions: list[chr]) -> None:
         self.atoms: Atoms = {prop: 1 for prop in propositions}
         self.constraints: list[Sentence] = []
-        self.interpretations: list[Interpretations] = []
-        self.models: list[Interpretations] = []
+        self.models: list[Interpretation] = []
+
+        self.interpretations: list[Interpretation] = list(
+            product([0, 1], repeat=len(self.atoms))
+        )
+
         self.operations: dict[chr, callable[[bool, bool], bool]] = {
             "¬": lambda p: not p,
             "⇒": lambda p, q: (not p) or q,
@@ -16,11 +22,15 @@ class BeliefBase:
             "∨": lambda p, q: p or q,
         }
 
-    def add_constraint(self, sentence: Sentence) -> None:
-        self.constraints.append(sentence)
-        self.get_models()
 
-    def evaluate_sentence(self, interpretation: list[int], sentence: Sentence) -> bool:
+    def add_constraint(self, sentence: Sentence) -> None:
+        """
+        This should actually add each new constraint in conjunction with the previous constraints
+        """
+        self.constraints.append(sentence)
+
+
+    def evaluate_sentence(self, interpretation: Interpretation, sentence: Sentence) -> bool:
         stack: list[chr] = []
         interp: Atoms = dict(zip(
             self.atoms.keys(), 
@@ -33,16 +43,22 @@ class BeliefBase:
                 second = stack.pop()
                 first = stack.pop()
 
-                stack.append(operation(first, second))
+                stack.append(operation(second, first))
             else:
                 stack.append(interp[char])
 
         return stack[0]
     
     def get_models(self) -> None:
-        return
+        for interp in self.interpretations:
+
+            # constraints shouldn't be a list!
+            if self.evaluate_sentence(interp, self.constraints[0]):
+                self.models.append(interp)
     
 
 # testing
 K = BeliefBase(["p", "q", "r"])
 K.add_constraint(["⇔", "r", "⇒", "p", "q"])
+K.get_models()
+K.models
