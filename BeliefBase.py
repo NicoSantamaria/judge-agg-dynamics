e1eeabf6ab9e7c0ffa2e1e51cb626052aa2b5522
NicoSantamaria <nicoasantamaria@gmail.com>
2024-10-04 19:40:33 -0700

    evaluate propositional sentence with stack algorithm

--- BeliefBase.py
+++ BeliefBase.py
@@ -1,11 +1,13 @@
 type Sentence = list[chr]
-type model = tuple[int]
+type Interpretations = tuple[int]
+type Atoms = dict[chr, int]
 
 class BeliefBase:
     def __init__(self, propositions: list[chr]) -> None:
-        self.atoms: list[chr] = propositions
+        self.atoms: Atoms = {prop: 1 for prop in propositions}
         self.constraints: list[Sentence] = []
-        self.models: list[model] = []
+        self.interpretations: list[Interpretations] = []
+        self.models: list[Interpretations] = []
         self.operations: dict[chr, callable[[bool, bool], bool]] = {
             "¬": lambda p: not p,
             "⇒": lambda p, q: (not p) or q,
@@ -19,7 +21,28 @@ class BeliefBase:
         self.get_models()
 
     def evaluate_sentence(self, interpretation: list[int], sentence: Sentence) -> bool:
-        return True
+        stack: list[chr] = []
+        interp: Atoms = dict(zip(
+            self.atoms.keys(), 
+            interpretation
+        ))
+
+        for char in sentence[::-1]:
+            if char in self.operations.keys():
+                operation = self.operations[char]
+                second = stack.pop()
+                first = stack.pop()
+
+                stack.append(operation(first, second))
+            else:
+                stack.append(interp[char])
+
+        return stack[0]
     
     def get_models(self) -> None:
         return
+    
+
+# testing
+K = BeliefBase(["p", "q", "r"])
+K.add_constraint(["⇔", "r", "⇒", "p", "q"])
