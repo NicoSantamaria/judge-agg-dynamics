41224c44da1d762274c3d59d97dfe4b4ab61d191
NicoSantamaria <nicoasantamaria@gmail.com>
2024-10-29 14:28:02 -0700

    hamming distance update rule

--- Graph.py
+++ Graph.py
@@ -9,10 +9,16 @@ type GraphEdges = Dict[Agent, List[Connection]]
 class Graph:
     def __init__(self, agenda: BeliefBase, agents: list[Agent]=list()) -> None:
         self.agenda: BeliefBase = agenda
-        self.graph: GraphEdges = {agent: [agent] for agent in agents}
+        self.graph: GraphEdges = {agent: [] for agent in agents}
 
     def add_connections(self, agent: Agent, connections: list[Connection]) -> None:
-        self.graph[agent] = connections 
+        self.graph[agent] = connections
+
+    def remove_connection(self, agent: Agent, connection: Agent) -> None:
+        return
+    
+    def complete_graph(self) -> None:
+        return
 
     def tiebreaker_chance(self, interps: List[Interpretation]) -> Interpretation:
         return random.choice(interps)
@@ -27,22 +33,43 @@ class Graph:
         return count
 
     
-    # def hamming_distance_rule(self, agent: Agent) -> List[Interpretation]:
-    #     candidates: List[Interpretation] = list()
+    def hamming_distance_rule(self, agent: Agent) -> List[Interpretation]:
+        candidates: List[Interpretation] = list()
+        candidate_minimum: int = len(self.agenda.atoms * len(self.graph))
 
-    #     for model in self.agenda.models:
-    #         for connection in self.graph[agent]:
-    #             break
+        for model in self.agenda.models:
+            current_distance: int = 0
 
-    #     return candidates
+            for connection in self.graph[agent]:
+                for agent_model in connection.models:
+                    current_distance += self.hamming_distance(model, agent_model)
+
+            print(model, current_distance)
+
+            if current_distance < candidate_minimum:
+                candidates = [model]
+                candidate_minimum = current_distance
+            elif current_distance == candidate_minimum:
+                candidates.append(model)
+
+        return candidates
 
 
 # testing
-IC = BeliefBase(["p", "q", "r", "s"], [["iff", "r", "and", "p", "q"], ['iff', 's', 'not', 'or', 'p', 'r']])
-K1 = Agent(IC, {"p": 0, "q": 0, "r": 0, "s": 1})
-K2 = Agent(IC, {"p": 0, "q": 1, "r": 0, "s": 1})
-G = Graph(IC, [K1, K2])
-G.add_connections(K1, [K2])
+# IC = BeliefBase(["p", "q", "r", "s"], [["iff", "r", "and", "p", "q"], ['iff', 's', 'not', 'or', 'p', 'r']])
+# K1 = Agent(IC, {"p": 0, "q": 0, "r": 0, "s": 1})
+# K2 = Agent(IC, {"p": 0, "q": 1, "r": 0, "s": 1})
+# G = Graph(IC, [K1, K2])
+# G.add_connections(K1, [K2])
 
 # for connection in G.graph[K1]:
 #     print(connection)
+
+IC = BeliefBase(["p", "q", "r"], [["iff", "r", "implies", "p", "q"]])
+K1 = Agent(IC, {"p": 1, "q": 1, "r": 1})
+K2 = Agent(IC, {"p": 1, "q": 0, "r": 0})
+K3 = Agent(IC, {"p": 0, "q": 0, "r": 1})
+G = Graph(IC, [K1, K2, K3])
+G.add_connections(K1, [K1, K2, K3])
+G.add_connections(K2, [K1, K2, K3])
+G.add_connections(K3, [K1, K2, K3])
