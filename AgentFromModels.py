4a284d5b951dacec0fe03429b5acb086fb86c538
NicoSantamaria <nicoasantamaria@gmail.com>
2025-02-13 15:25:35 -0800

    update beliefs fn for model-based agent

--- AgentFromModels.py
+++ AgentFromModels.py
@@ -6,3 +6,6 @@ class AgentFromModels:
     def __init__(self, model: Interpretation, name: str="") -> None:
         self.model = model
         self.name = name
+
+    def update_beliefs(self, model: Interpretation):
+        self.model = model
