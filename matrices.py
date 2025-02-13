0b18531f547be2dc60260406a3ad91c585c0fae6
NicoSantamaria <nicoasantamaria@gmail.com>
2025-02-13 14:43:59 -0800

    markov chains with numpy

--- matrices.py
+++ matrices.py
@@ -1,18 +1,34 @@
 import numpy as np
 from functools import reduce
+from Graph import *
+from typing import *
 
-a = np.array([[1,0,1/3],
-              [0,1/2,1/3],
-              [0,1/2,1/3]])
+type Matrix = np.ndarray
+type StateGraph = Dict[Graph, List[Connection]]
 
-eigenvalues, trans_matrix = np.linalg.eig(a)
-diagonal = np.diag(eigenvalues)
-trans_matrix_inv = np.linalg.inv(trans_matrix)
+class MarkovChain:
+    def __init__(self, graph: Graph):
+        self.graph: Graph = graph
+        self.state_graph: StateGraph = self.generate_states()
 
-first = np.linalg.matrix_power(a, 1000)
-second = reduce(np.matmul, [trans_matrix, np.linalg.matrix_power(diagonal, 1000), trans_matrix_inv])
+    def generate_states() -> StateGraph:
+        return
+    
+    def fast_exponent(mat: Matrix) -> Matrix:
+        eigenvalues, eigenvectors = np.linalg.eig(mat)
+        diagonal = np.diag(eigenvalues)
+        trans_matrix_inv = np.linalg.inv(eigenvectors)
 
-print(first)
-print(second)
+        return reduce(np.matmul, [
+            eigenvectors, 
+            np.linalg.matrix_power(diagonal, 1000), 
+            trans_matrix_inv
+        ])
 
-# it works!
+    def find_stationary(mat: Matrix) -> Matrix:
+        eigenvalues, eigenvectors = np.linalg.eig(mat)
+        stationary_index = np.where(np.isclose(eigenvalues, 1))[0][0]
+        stationary_distribution = eigenvectors[:, stationary_index]
+        stationary_distribution /= np.sum(stationary_distribution)
+
+        return stationary_distribution
