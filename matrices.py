7fa3e5b3a1160a01c58e0595184e1496ca9e6daa
NicoSantamaria <nicoasantamaria@gmail.com>
2025-02-13 16:42:50 -0800

    efficient update rule for GraphFromModels

--- matrices.py
+++ matrices.py
@@ -8,14 +8,14 @@ from GraphFromModels import *
 from copy import deepcopy
 
 type Matrix = np.ndarray
-type StateGraph = Dict[GraphFromModels, List[Connection]]
+type StateGraphMatrix = List[List[int]]
 type State = List[Tuple[str, Interpretation]]
 
 class MarkovChain:
     def __init__(self, graph: GraphFromModels):
         self.graph: GraphFromModels = graph
         self.states: List[State] = self.generate_states()
-        self.state_graph: StateGraph = self.build_state_graph()
+        self.state_graph_matrix: StateGraphMatrix = self.build_state_graph()
 
     def generate_states(self) -> List[State]:
         # number of states can be easliy computed beforehand, so this can
@@ -23,33 +23,50 @@ class MarkovChain:
         # later on
         states: List[GraphFromModels] = list()
         
-        models_combos = product(self.graph.models, repeat=len(self.graph.graph))
         agents_perms = permutations(self.graph.graph)
+        models_combos = product(
+            self.graph.models, 
+            repeat=len(self.graph.graph)
+        )
         
         for agents, models in product(agents_perms, models_combos):
-            new_state: State = [(agent.name, model) for agent, model in zip(agents, models)]
+            new_state: State = [(agent.name, model) 
+                for agent, model in zip(agents, models)
+            ]
             states.append(new_state)
 
         return states
     
-    def build_state_graph(self) -> StateGraph:
-        state_graph = dict()
-        """
-        How this will work:
+    def build_state_graph(self) -> StateGraphMatrix:
+        length = len(self.states)
+        state_graph_matrix = [[0] * length] * length
 
-        First, construct a copy of the original graph. Use
-        the copy for the next operations
+        # iterate over the states
+        for i, state in enumerate(self.states):
+            print(state)
 
-        For each state in self.states:
-            1. update all of the agents in the graph to the 
-            the beliefs in the state
-            2. call the update rule, producing all the best
-            tied outcomes
-        
-        Need to think about a good data structure to store all 
-        the information, and an efficient way to compute
-        """
-        return 
+        return state_graph_matrix
+    
+    # works, as far as I can tell
+    def hamming_distance_rule(self, models: List[Interpretation]) -> List[Interpretation]:
+        candidates: List[Interpretation] = self.graph.models
+        candidate_minimum: float = float('inf')
+
+        for candidate in candidates:
+            current_distance: int = 0
+
+            for model in models:
+                distance_to_model = self.graph.hamming_distance(candidate, model)
+                current_distance += distance_to_model
+
+            if current_distance < candidate_minimum:
+                candidates = [candidate]
+                candidate_minimum = current_distance
+            elif current_distance == candidate_minimum:
+                candidates.append(candidate)
+
+        return candidates
+    
     
     def fast_exponent(self, mat: Matrix) -> Matrix:
         eigenvalues, eigenvectors = np.linalg.eig(mat)
@@ -70,17 +87,17 @@ class MarkovChain:
 
         return stationary_distribution
     
-# props = ['p', 'q', 'r']
-# I = BeliefBase(props, [['iff', 'r', 'implies', 'p', 'q']])
+props = ['p', 'q', 'r']
+I = BeliefBase(props, [['iff', 'r', 'implies', 'p', 'q']])
 
-# J1 = AgentFromModels((1, 0, 0), 'A')
-# J2 = AgentFromModels((1, 1, 1), 'B')
-# J3 = AgentFromModels((0, 0, 1), 'C')
+J1 = AgentFromModels((1, 0, 0), 'A')
+J2 = AgentFromModels((1, 1, 1), 'B')
+J3 = AgentFromModels((0, 0, 1), 'C')
 
-# G = GraphFromModels(I.models, [J1, J2, J3])
+G = GraphFromModels(I.models, [J1, J2, J3])
 
-# G.add_connections(J1, [J1, J2, J3])
-# G.add_connections(J2, [J1, J2])
-# G.add_connections(J3, [J3])
+G.add_connections(J1, [J1, J2, J3])
+G.add_connections(J2, [J1, J2])
+G.add_connections(J3, [J3])
 
-# MC = MarkovChain(G)
+MC = MarkovChain(G)
