a875bac2127733625fb0b4b27daff8506048f73f
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-25 17:35:44 -0700

    test get_models method

--- tests/test_belief_base.py
+++ tests/test_belief_base.py
@@ -12,3 +12,16 @@ def test_get_constraints():
 
     K = BeliefBase(["p", "q", "r"], [["not", "p"], ["or", "p", "r"], ["implies", "p", "q"]])
     assert K.constraints == ["and", "and", "not", "p", "or", "p", "r", "implies", "p", "q"]
+
+def test_get_models():
+    K = BeliefBase(["p", "q"], [])
+    assert set(K.models) == {(0, 0), (0, 1), (1, 0), (1, 1)}
+
+    K = BeliefBase(["p", "q"], [["and", "p", "not", "p"]])
+    assert set(K.models) == set()
+
+    K = BeliefBase(["p", "q", "r"], [["iff", "r", "implies", "p", "q"]])
+    assert set(K.models) == {(1, 1, 1), (0, 0, 1), (1, 0, 0), (0, 1, 1)}
+
+    K = BeliefBase(["p", "q", "r"], [["not", "p"], ["or", "p", "r"]])
+    assert set(K.models) == {(0, 1, 1), (0, 0, 1)}
