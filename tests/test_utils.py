a5cd9a8fba1547b3ec2f8d25bfe15d3e974117c7
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-26 19:57:01 -0700

    add tests for hamming_distance

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,6 +7,9 @@ def test_hamming_distance():
     assert hamming_distance([], []) == 0
     assert hamming_distance([Z2(1)], [Z2(1)]) == 0
     assert hamming_distance([Z2(1), Z2(0)], [Z2(0), Z2(1)]) == 2
+    assert hamming_distance([Z2(1), Z2(0), Z2(0),Z2(1)], [Z2(1), Z2(1), Z2(0),Z2(1)]) == 1
 
     with pytest.raises(ValueError, match="List lengths are not equal."):
         hamming_distance([Z2(1)], [Z2(1), Z2(0)])
+    with pytest.raises(ValueError, match="List lengths are not equal."):
+        hamming_distance([], [Z2(1), Z2(0), Z2(1)])
