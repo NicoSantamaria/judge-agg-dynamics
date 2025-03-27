772f759d6a5b6ca9b8a5f42ccef46ff0069e75e4
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-26 22:25:30 -0700

    move BeliefBase to new types and enums

--- src/BeliefBase.py
+++ src/BeliefBase.py
@@ -1,88 +1,36 @@
 from itertools import product
-from typing import List, Dict, Callable
+from typing import List
+from utils.utils import evaluate_sentence
 from utils.enums import Z2, Prop, Logic
-from utils.types import Sentence
+from utils.types import Sentence, Interpretation
 
 
 class BeliefBase:
     def __init__(self, atoms: List[Prop], constraints: List[Sentence]=list(),) -> None:
-        # the atomic propositions with interpretations.
-        # -1 represents a "no judgment"
         self.atoms: List[Prop] = atoms
+        self.constraints: Sentence = self.get_constraints(constraints)
+        self.models: List[Interpretation] = self.get_models()
 
-        # the standard logical operations
-        self.operations: Dict[str, Callable] = {
-            "not": lambda p: not p,
-            "implies": lambda p, q: (not p) or q,
-            "iff": lambda p, q: p == q,
-            "and": lambda p, q: p and q,
-            "or": lambda p, q: p or q,
-        }
-
-        # computes the set of logical constraints as a single conjunctive
-        # propositional sentence; this makes it easier to find models
-        self.constraints = self.get_constraints(constraints)
-
-        # computes all the combinations of truth-values on the atomic propositions
-        # that satisfy the constraints: stores the models as binary tuples
-        self.models: list[Interpretation] = self.get_models()
-
-    def get_constraints(self, constraints: list[Sentence]) -> Sentence:
-        # build the single conjunctive sentence from the list of
-        # given constraints.
+    def get_constraints(self, constraints: List[Sentence]) -> Sentence:
         if not constraints:
             return list()
 
         constraint_sentence: Sentence = constraints[0]
-
         for sentence in constraints[1:]:
             constraint_sentence = self.get_conjunction(
                 constraint_sentence,
                 sentence
             )
-
         return constraint_sentence
 
     def get_conjunction(self, sentence1: Sentence, sentence2: Sentence) -> Sentence:
-        # represent the conjunction of two sentences in Polish Notation
-        return ["and"] + sentence1 + sentence2
-
-
-    def evaluate_sentence(self, interpretation: Interpretation, sentence: Sentence) -> bool:
-        # evaluate the truth value of a propositional sentence given an interpretation
-        # of the atomic propositions, e.g., (1,0) on (p, q) yields false for
-        # 'p implies q'
-        stack: List[str] = list()
-        interp: Beliefs = dict(zip(
-            self.atoms,
-            interpretation
-        ))
-
-        # evaluate the polish notation using the stack
-        for char in reversed(sentence):
-            if char in self.operations.keys():
-                operation = self.operations[char]
-
-                if char == "not":
-                    first = stack.pop()
-                    stack.append(operation(first))
-                else:
-                    second = stack.pop()
-                    first = stack.pop()
-                    stack.append(operation(second, first))
-            else:
-                stack.append(interp[char])
-
-        return stack[0]
+        return [Logic.AND] + sentence1 + sentence2
 
 
     def get_models(self) -> List[Interpretation]:
-        # compute all the combinations of truth-values for the atomic
-        # propositions which satisfy the constraints
         models: List[Interpretation] = list()
-
-        for interp in product([0, 1], repeat=len(self.atoms)):
-            if not self.constraints or self.evaluate_sentence(interp, self.constraints):
-                models.append(interp)
-
+        for interp in product([Z2(0), Z2(1)], repeat=len(self.atoms)):
+            interp_list = list(interp)
+            if not self.constraints or evaluate_sentence(self.atoms, interp_list, self.constraints):
+                models.append(interp_list)
         return models
--- tests/test_belief_base.py
+++ tests/test_belief_base.py
@@ -1,27 +1,31 @@
-# from src.BeliefBase import BeliefBase
+from src.BeliefBase import BeliefBase
+from utils.types import Logic, Prop, Z2
 
-# def test_get_constraints():
-#     K = BeliefBase(["p", "q"], [])
-#     assert K.constraints == []
+def test_get_constraints():
+    K = BeliefBase([Prop.P, Prop.Q], [])
+    assert K.constraints == []
 
-#     K = BeliefBase(["p", "q", "r"], [["iff", "r", "implies", "p", "q"]])
-#     assert K.constraints == ["iff", "r", "implies", "p", "q"]
+    K = BeliefBase([Prop.P, Prop.Q, Prop.R], [[Logic.IFF, Prop.R, Logic.IMPLIES, Prop.P, Prop.Q]])
+    assert K.constraints == [Logic.IFF, Prop.R, Logic.IMPLIES, Prop.P, Prop.Q]
 
-#     K = BeliefBase(["p", "q"], [["and", "p", "q"], ["not", "q"]])
-#     assert K.constraints == ["and", "and", "p", "q", "not", "q"]
+    K = BeliefBase([Prop.P, Prop.Q], [[Logic.AND, Prop.P, Prop.Q], [Logic.NOT, Prop.Q]])
+    assert K.constraints == [Logic.AND, Logic.AND, Prop.P, Prop.Q, Logic.NOT, Prop.Q]
 
-#     K = BeliefBase(["p", "q", "r"], [["not", "p"], ["or", "p", "r"], ["implies", "p", "q"]])
-#     assert K.constraints == ["and", "and", "not", "p", "or", "p", "r", "implies", "p", "q"]
+    K = BeliefBase([Prop.P, Prop.Q, Prop.R], [[Logic.NOT, Prop.P], [Logic.OR, Prop.P, Prop.R], [Logic.IMPLIES, Prop.P, Prop.Q]])
+    assert K.constraints == [Logic.AND, Logic.AND, Logic.NOT, Prop.P, Logic.OR, Prop.P, Prop.R, Logic.IMPLIES, Prop.P, Prop.Q]
 
-# def test_get_models():
-#     K = BeliefBase(["p", "q"], [])
-#     assert set(K.models) == {(0, 0), (0, 1), (1, 0), (1, 1)}
+def test_get_models():
+    def make_set(models):
+        return set(tuple(model) for model in K.models)
 
-#     K = BeliefBase(["p", "q"], [["and", "p", "not", "p"]])
-#     assert set(K.models) == set()
+    K = BeliefBase([Prop.P, Prop.Q], [])
+    assert make_set(K.models) == make_set([[Z2(0), Z2(0)], [Z2(0), Z2(1)], [Z2(1), Z2(0)], [Z2(1), Z2(1)]])
 
-#     K = BeliefBase(["p", "q", "r"], [["iff", "r", "implies", "p", "q"]])
-#     assert set(K.models) == {(1, 1, 1), (0, 0, 1), (1, 0, 0), (0, 1, 1)}
+    K = BeliefBase([Prop.P, Prop.Q], [[Logic.AND, Prop.P, Logic.NOT, Prop.P]])
+    assert make_set(K.models) == set()
 
-#     K = BeliefBase(["p", "q", "r"], [["not", "p"], ["or", "p", "r"]])
-#     assert set(K.models) == {(0, 1, 1), (0, 0, 1)}
+    K = BeliefBase([Prop.P, Prop.Q, Prop.R], [[Logic.IFF, Prop.R, Logic.IMPLIES, Prop.P, Prop.Q]])
+    assert make_set(K.models) == make_set([[Z2(1), Z2(1), Z2(1)], [Z2(0), Z2(0), Z2(1)], [Z2(1), Z2(0), Z2(0)], [Z2(0), Z2(1), Z2(1)]])
+
+    K = BeliefBase([Prop.P, Prop.Q, Prop.R], [[Logic.NOT, Prop.P], [Logic.OR, Prop.P, Prop.R]])
+    assert make_set(K.models) == make_set([[Z2(0), Z2(1), Z2(1)], [Z2(0), Z2(0), Z2(1)]])
