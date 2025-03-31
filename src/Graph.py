63ed50c8cee82ab6d1785170b63069f1f5b218e9
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-30 21:50:27 -0700

    add agents to graph

--- src/Graph.py
+++ src/Graph.py
@@ -1,27 +1,37 @@
 import random
 from typing import List
 from itertools import product
+from utils.utils import hamming_distance
 from utils.types import Interpretation, Connection
 from src.BeliefBase import BeliefBase
 
 
 class Graph:
-    def __init__(self, models: List[Interpretation] | BeliefBase, connections: List[Connection]) -> None:
+    def __init__(self,
+        models: List[Interpretation] | BeliefBase,
+        connections: List[Connection],
+        agents: List[Interpretation]=[]
+    ) -> None:
         self.models: List[Interpretation] = []
         if isinstance(models, BeliefBase):
             self.models = models.models
         else:
             self.models = models
 
+        for agent in agents:
+            if agent not in self.models:
+                raise ValueError("Agents must be represented by models.")
+        self.agents = agents
+
         for model1, model2 in connections:
-            if model1 not in self.models or model2 not in self.models:
-                raise ValueError("Connections can only be drawn between models.")
+            if model1 not in self.agents or model2 not in self.agents:
+                raise ValueError("Connections can only be drawn between agents.")
         self.connections: List[Connection] = connections
 
     def add_connections(self, connection: Connection) -> None:
         (model1, model2) = connection
         if model1 not in self.models or model2 not in self.models:
-            raise ValueError("Connections can only be drawn between models.")
+            raise ValueError("Connections can only be drawn between agents.")
         self.connections.append(connection)
 
     def remove_connection(self, connection: Connection) -> None:
@@ -32,50 +42,43 @@ class Graph:
     def complete_graph(self) -> None:
         self.connections = [(a, b) for a, b in product(self.models, repeat=2)]
 
+    # def update(self) -> None:
+    #     for i, agent in enumerate(self.agents):
+    #         candidates = self.hamming_distance_rule(agent)
+    #         result = self.tiebreaker_chance(candidates)
+    #         self.agents[i] = result
 
-    def update(self) -> None:
-        results = []
+    # def hamming_distance_rule(self, agent: Interpretation) -> List[Interpretation]:
+    #     candidates: List[Interpretation] = list()
+    #     candidate_minimum: float = float('inf')
 
-        for agent in self.graph:
-            candidates = self.hamming_distance_rule(agent)
-            result = self.tiebreaker_chance(candidates)
-            results.append(result)
+    #     for model in self.models:
+    #         current_distance: int = 0
 
-        for i, agent in enumerate(self.graph):
-            agent.update_beliefs(results[i])
+    #         for connection in self.graph[agent]:
+    #             distance_to_agent_model = hamming_distance(model, connection.model)
+    #             current_distance += distance_to_agent_model
 
+    #         if current_distance < candidate_minimum:
+    #             candidates = [model]
+    #             candidate_minimum = current_distance
+    #         elif current_distance == candidate_minimum:
+    #             candidates.append(model)
 
-    def hamming_distance_rule(self, agent: AgentFromModels) -> List[Interpretation]:
-        candidates: List[Interpretation] = list()
-        candidate_minimum: float = float('inf')
-
-        for model in self.models:
-            current_distance: int = 0
-
-            for connection in self.graph[agent]:
-                distance_to_agent_model = hamming_distance(model, connection.model)
-                current_distance += distance_to_agent_model
-
-            if current_distance < candidate_minimum:
-                candidates = [model]
-                candidate_minimum = current_distance
-            elif current_distance == candidate_minimum:
-                candidates.append(model)
-
-        return candidates
+    #     return candidates
 
 
-    def tiebreaker_chance(self, interps: List[Interpretation]) -> Interpretation:
-        return random.choice(interps)
+    # def tiebreaker_chance(self, interps: List[Interpretation]) -> Interpretation:
+    #     return random.choice(interps)
 
 
-    def __str__(self):
-        result = "The graph contains the following agents and their models:\n"
+    # def __str__(self):
+    #     result = "The graph contains the following agents and their models:\n"
 
-        for agent, connections in self.graph.items():
-            result += f"Agent {agent.name} with models {agent.model}:\n"
+    #     for agent, connections in self.graph.items():
+    #         result += f"Agent {agent.name} with models {agent.model}:\n"
 
-            for connection in connections:
-                result += f"  Connected to: {connection.name}, Models: {connection.model}\n"
+    #         for connection in connections:
+    #             result += f"  Connected to: {connection.name}, Models: {connection.model}\n"
 
-        return result
+    #     return result
