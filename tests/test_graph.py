fc6578d793ff1a5c401c7f1be381c635f8c11778
NicoSantamaria <nicoasantamaria@gmail.com>
2025-03-30 18:42:53 -0700

    test exceptions on graph init

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,3 +1,4 @@
+import pytest
 from typing import List
 from src.Graph import Graph
 from src.BeliefBase import BeliefBase
@@ -40,3 +41,28 @@ def test_init():
     connections: List[Connection] = [(K.models[0], K.models[1]), (K.models[0], K.models[2])]
     G = Graph(K, connections)
     assert G.models == K.models
+
+    models: List[Interpretation] = []
+    connections: List[Connection] = [([Z2(0)], [Z2(1)])]
+    with pytest.raises(ValueError, match="Connections can only be drawn between models."):
+        G = Graph(models, connections)
+
+    models: List[Interpretation] = [[Z2(1), Z2(0)], [Z2(0), Z2(1)]]
+    connections: List[Connection] = [([Z2(0)], [Z2(1)])]
+    with pytest.raises(ValueError, match="Connections can only be drawn between models."):
+        G = Graph(models, connections)
+
+    models: List[Interpretation] = [[Z2(1), Z2(0)], [Z2(0), Z2(1)]]
+    connections: List[Connection] = [([Z2(0), Z2(0)], [Z2(1), Z2(0)])]
+    with pytest.raises(ValueError, match="Connections can only be drawn between models."):
+        G = Graph(models, connections)
+
+    models: List[Interpretation] = [[Z2(1), Z2(0)], [Z2(0), Z2(1)]]
+    connections: List[Connection] = [([Z2(1), Z2(0)], [Z2(1), Z2(1)])]
+    with pytest.raises(ValueError, match="Connections can only be drawn between models."):
+        G = Graph(models, connections)
+
+    K = BeliefBase([Prop.P, Prop.Q, Prop.R], [[Logic.IFF, Prop.R, Logic.IMPLIES, Prop.P, Prop.Q]])
+    connections: List[Connection] = [([Z2(1), Z2(0), Z2(1)], [Z2(1), Z2(1), Z2(1)])]
+    with pytest.raises(ValueError, match="Connections can only be drawn between models."):
+        G = Graph(K, connections)
