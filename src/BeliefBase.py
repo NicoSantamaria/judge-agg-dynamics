739ef551c0d2e2a091ff27d7101a5791a20e57bc
NicoSantamaria <nicoasantamaria@gmail.com>
2025-04-08 22:02:05 -0700

    docstring for BeliefBase class

--- src/BeliefBase.py
+++ src/BeliefBase.py
@@ -7,6 +7,24 @@ from utils.types import Sentence, Interpretation
 
 # TODO: allow input by strs instead of props
 class BeliefBase:
+    """
+    A `belief base` contains a set of atomic propositions, a set of rational
+    constraints on those propositions called `integrity constraints,` and a set
+    of vectors over Z_2 representing rational assignments of truth values to the
+    atomic propositions with respect to the integrity constraints.
+
+    ATTRIBUTES:
+        atoms (List[Prop]): The atomic propositions in the agenda.
+        constraints (Sentence): The conjunction of propositional sentence in the
+        set of integrity constraints.
+        models (List[Interpretation]): The vectors representing rational assignments
+        of truth values to the atomic propositions with respect to the integrity constraints
+
+    REFERENCES:
+    [1] Gabriella Pigozzi. Belief merging and the discursive dilemma: an
+            argument-based account to paradoxes of judgment aggregation. Synthese,
+            152(2):285–98, 2006.
+    """
     def __init__(self, atoms: List[Prop], constraints: List[Sentence]=list()) -> None:
         self.atoms: List[Prop] = atoms
         self.constraints: Sentence = self.get_constraints(constraints)
