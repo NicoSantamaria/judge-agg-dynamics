c09219a2414a31c8208d59a307fe03f1e18bfdff
NicoSantamaria <nicoasantamaria@gmail.com>
2025-04-13 12:05:30 -0700

    docstring for experiment1

--- experiments/experiment1.py
+++ experiments/experiment1.py
@@ -4,6 +4,19 @@ from src.MarkovChain import MarkovChain
 from src.Graph import Graph
 
 def run_experiment():
+    """
+    CONJECTURE: For each agent in a graph, the number of most frequent
+    models after many iterations will always be less than or equal to
+    the number of models that might be adopted after a single iteration
+    of the distance-based rule. In this way, the Markov Chain analysis
+    narrows the space of reasonable collective judgments.
+
+    DESCRIPTION: Simple graph example presented in thesis as example.
+
+    FINDINGS: Yes. After many iterations, the most frequent belief for all
+    of the 3 agents is the belief (0, 0, 1), where each agent could adopt
+    one or more beliefs after a single iteration of the update rule. 
+    """
     K = BeliefBase(
         [Prop.P, Prop.Q, Prop.R],
         [[Logic.IFF, Prop.R, Logic.IMPLIES, Prop.P, Prop.Q]]
